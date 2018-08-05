68dd2ce3d76a3a54ae1a150a9b6879f289b000d2
Yin <yin.codes@gmail.com>
2018-08-05 01:30:49 -0400

    hashed the associated_data in order to authenticate it in main.py

--- aesgcm.py
+++ aesgcm.py
@@ -1,12 +1,16 @@
 import os
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.ciphers import (
+    Cipher, algorithms, modes
+)
 
-def encrypt(key, message, auth):
+def encrypt(key, plaintext, associated_data):
   aesgcm = AESGCM(key)
-  nonce = os.urandom(16)
-  print("nonce:", type(nonce))
-  ciphertext = aesgcm.encrypt(nonce, message, auth)
+  nonce = os.urandom(16) #never reuse a nonce with a key
+  #associated_data is authenticated with the key but not encrypted.
+  ciphertext = aesgcm.encrypt(nonce, plaintext, associated_data)
   return ([aesgcm, nonce, ciphertext])
 
-def decrypt(aesgcm, nonce, ciphertext, auth):
-  return aesgcm.decrypt(nonce, ciphertext, auth)
+def decrypt(aesgcm, nonce, ciphertext, associated_data):
+  return aesgcm.decrypt(nonce, ciphertext, associated_data)
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import key_gen
 import aesgcm
 import twilio_sms
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, hmac
+
 
 def utf8len(s):
     return len(s.encode('utf-8'))
+
+def hash(key, message_to_hash):
+  h = hmac.HMAC(key, hashes.SHA256(), backend=default_backend())
+  h.update(message_to_hash)
+  return h.finalize()
 
 def main():
   #Welcome
@@ -21,10 +29,11 @@ def main():
 
   print("type of message:", type(message))
   
-  #This is just information associated to the user
+  #This is just information associated to the user, authenticated with the key but not encrypted.
   associated_data = b"This is the user_id, unique to the user from the company server"
+  authenticated_associated_data = hash(derived_key, associated_data)
 
-  encrypted_packet = aesgcm.encrypt(derived_key, message, associated_data)
+  encrypted_packet = aesgcm.encrypt(derived_key, message, authenticated_associated_data)
   ciphertext = encrypted_packet[2]
   aesgcm_nonce = encrypted_packet[1]
   aesgcm_key = encrypted_packet[0]
@@ -35,7 +44,7 @@ def main():
   #plaintext = twilio_sms.receive(ciphertext)
   #twilio_sms.send(plaintext)
 
-  plaintext = aesgcm.decrypt(aesgcm_key, aesgcm_nonce, ciphertext, associated_data)
+  plaintext = aesgcm.decrypt(aesgcm_key, aesgcm_nonce, ciphertext, authenticated_associated_data)
   print(receiver, "got", plaintext)
 
 main()
